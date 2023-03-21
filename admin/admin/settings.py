d8228bbabc1862c93b4d767a558b0d95a7b3d5ef
Birane SAMB <78302571+biranesamb9@users.noreply.github.com>
2023-03-21 12:56:32 +0100

    remove mot de passe

--- admin/admin/settings.py
+++ admin/admin/settings.py
@@ -30,8 +30,8 @@ SECRET_KEY = 'django-insecure-_wf9vr@s1ux!%8*^2wtl+n%l!hj34#)km^ehjjzb9#zmm&@(w=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'biranesamb9@gmail.com'
-EMAIL_HOST_PASSWORD = 'rlctoytxpdztdkkc'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD =
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
